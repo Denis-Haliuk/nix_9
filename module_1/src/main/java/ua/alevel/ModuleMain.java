087f8497cffd7e599308709351d804bda918f6ec
undef1n3d-git <41990012+undef1n3d-git@users.noreply.github.com>
2021-10-25 22:12:06 +0300

    module_1 started task1 ready

---
+++ module_1/src/main/java/ua/alevel/ModuleMain.java
@@ -0,0 +1,15 @@
+package ua.alevel;
+
+import ua.alevel.uniquesymbols.UniqueSymbolsInArray;
+
+public class ModuleMain {
+    public static void main(String[] args) {
+        System.out.println("ModuleMain.main");
+        System.out.println("Task 1 - find unique symbols in array");
+        UniqueSymbolsInArray uniqueSymbolsInArray = new UniqueSymbolsInArray();
+        uniqueSymbolsInArray.printUniqueSymbolsInArray();
+
+
+
+    }
+}
---
+++ module_1/src/main/java/ua/alevel/uniquesymbols/UniqueSymbolsInArray.java
@@ -0,0 +1,22 @@
+package ua.alevel.uniquesymbols;
+
+public class UniqueSymbolsInArray {
+    public void printUniqueSymbolsInArray() {
+        int[] numArray = {1, 4, 5, 1, 1, 3};
+
+        for (int number : numArray) {
+            System.out.print(number + " ");
+        }
+        System.out.println();
+        int unique = 0;
+        for (int i = 0; i <
+                numArray.length; i++) {
+            boolean isUnique = true;
+            for (int j = i + 1; j < numArray.length; j++) {
+                isUnique = isUnique && (numArray[i] != numArray[j]);
+            }
+            if (isUnique) unique++;
+        }
+        System.out.println("unique = " + unique);
+    }
+}
